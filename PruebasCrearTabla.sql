67a52f0dd68d88aeadaa3fad0bd8326380ee7c20
NahuelSavedra <61848207+NahuelSavedra@users.noreply.github.com>
2022-11-10 14:03:13 -0300

    evaluacion de schema

--- PruebasCrearTabla.sql
+++ PruebasCrearTabla.sql
@@ -1,10 +1,12 @@
 DECLARE @BaseOrigen VARCHAR(30)= 'DB_BaseOrigen';
 DECLARE @BaseDestino VARCHAR(30)= 'DB_BaseDestino';
 			DECLARE @Nombre_Tabla VARCHAR(500);
+			DECLARE @Schema VARCHAR(500);
+			declare @SchemaResult varchar(500) = '';
 			DECLARE @Nombre_Columna VARCHAR(500);
 
 			DECLARE @SqlDinamico NVARCHAR(MAX)
-			DECLARE @Crear_Tabla NVARCHAR(MAX)
+			DECLARE @SchemaQuery NVARCHAR(MAX);
 
 --recorre las tablas de la bd 1 
 SET @SqlDinamico = 'DECLARE Table_Cursor CURSOR FOR SELECT name FROM '+@BaseOrigen+'.sys.tables';
@@ -13,8 +15,12 @@ SET @SqlDinamico = 'DECLARE Table_Cursor CURSOR FOR SELECT name FROM '+@BaseOrig
 					FETCH NEXT FROM Table_Cursor INTO @Nombre_Tabla; 
 					WHILE @@FETCH_STATUS = 0 
 					BEGIN
+						--Guardo el Schema en una variable
+						SET @SchemaQuery = N'SELECT @Schema = TABLE_SCHEMA FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ' + CHAR(39) + @Nombre_Tabla + CHAR(39);
+						EXECUTE SP_EXECUTESQL @SchemaQuery, N'@Schema varchar(500) out', @SchemaResult out
 						--Le paso las tablas para que compare las bases
-						EXEC sp_CompareTables @Nombre_Tabla, @BaseOrigen, @BaseDestino
+
+						EXEC sp_CompareTables @Nombre_Tabla, @BaseOrigen, @BaseDestino, @SchemaResult
 
 					FETCH NEXT FROM Table_Cursor INTO @Nombre_Tabla; 
 					END
@@ -25,7 +31,7 @@ GO
 
 DROP PROCEDURE sp_CompareTables
 
-CREATE PROCEDURE sp_CompareTables @Nombre_Tabla VARCHAR(50),@BaseOrigen  VARCHAR(50), @BaseDestino VARCHAR(50)
+CREATE PROCEDURE sp_CompareTables @Nombre_Tabla VARCHAR(50),@BaseOrigen  VARCHAR(50), @BaseDestino VARCHAR(50), @Schema VARCHAR(50) = NULL
 AS
 DECLARE @Query_Base_Destino VARCHAR(50),
 		@Tabla_Base_Destino NVARCHAR(MAX),
@@ -37,20 +43,42 @@ DECLARE @Query_Base_Destino VARCHAR(50),
 		@Longitud VARCHAR(50);
 				
 --Armo la query para ver si existe la tabla en la base destino (Hay que contemplar si tiene schema)
-SET @Query_Base_Destino = 'SELECT ' + @Nombre_Tabla + ' FROM ' + @BaseDestino + '.sys.tables';
+IF @Schema IS NULL
+	BEGIN
+		SET @Query_Base_Destino = 'SELECT * FROM ' + @BaseDestino + '.' + @Nombre_Tabla;
+	END
+ELSE 
+	BEGIN
+		SET @Query_Base_Destino = 'SELECT * FROM ' + @BaseDestino + '.' + @Schema + '.' + @Nombre_Tabla;
+END
+
 	BEGIN TRY 
 		--Trato de ejecutar la query, si no existe devuelve error (pasa al bloque CATCH)
+		SELECT @Query_Base_Destino
 		EXEC SP_EXECUTESQL @Query_Base_Destino
 		--Si existe hay que ejecutar procedimiento para comprobar que las columnas sean iguales
 	END TRY
 	BEGIN CATCH
+		INSERT INTO Errores (mensaje) values ( 'La tabla ' + @Nombre_Tabla + ' no existe en ' + @BaseDestino)
 		--Armo un cursor para recorrer los campos que tiene la tabla de la base origen 
 		SET @CursorColumnas = 'DECLARE CursorColumnas CURSOR FOR SELECT COLUMN_NAME, DATA_TYPE,CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ' + CHAR(39) + @Nombre_Tabla + CHAR(39);
+		PRINT 'USE ' + @BaseDestino;
 		EXECUTE SP_EXECUTESQL @CursorColumnas;
 		OPEN CursorColumnas;
 			--Arma la query para crear tabla si no existe
 			FETCH NEXT FROM CursorColumnas INTO @Nombre_Columna, @Tipo_Columna, @Longitud;
-			SET @CrearTabla = N'CREATE TABLE [' + @BaseDestino + '].[' + @Nombre_Tabla + '] ( [' + @Nombre_Columna + '] [' + @Tipo_Columna + ']';
+			--Chequeo si tiene Schema
+			IF @Schema IS NULL
+				BEGIN
+					SET @CrearTabla = 'CREATE TABLE [' + @Nombre_Tabla + '] ( [' + @Nombre_Columna + '] [' + @Tipo_Columna + '],';
+				END
+			ELSE 
+				BEGIN
+					PRINT 'CREATE SCHEMA ' + @Schema;
+					SET @CrearTabla = 'CREATE TABLE [' + @Schema +'].[' + @Nombre_Tabla + '] ( [' + @Nombre_Columna + '] [' + @Tipo_Columna + '],';
+				END
+
+			--Si el campo es int la longitud es null
 			IF @Longitud IS NULL
 				BEGIN
 					PRINT @CrearTabla
@@ -65,11 +93,11 @@ SET @Query_Base_Destino = 'SELECT ' + @Nombre_Tabla + ' FROM ' + @BaseDestino + 
 					SET @CrearTabla = '[' + @Nombre_Columna + '] [' + @Tipo_Columna + ']';
 						IF @Longitud IS NULL
 							BEGIN
-								PRINT @CrearTabla
+								PRINT @CrearTabla + ','
 							END
 						ELSE 
 								BEGIN
-									PRINT @CrearTabla + ' (' + @Longitud + ')';
+									PRINT @CrearTabla + ' (' + @Longitud + '),';
 								END
 			FETCH NEXT FROM CursorColumnas INTO @Nombre_Columna, @Tipo_Columna, @Longitud;
 			END
@@ -78,4 +106,6 @@ SET @Query_Base_Destino = 'SELECT ' + @Nombre_Tabla + ' FROM ' + @BaseDestino + 
 		DEALLOCATE CursorColumnas;
 	END CATCH
 
-EXEC sp_CompareTables 'Dueño', 'DB_BaseOrigen' ,'DB_BaseDestino'
+--fin SP
+
+EXEC sp_CompareTables 'Dueño', 'DB_BaseOrigen' ,'DB_BaseDestino', 'Test'
--- TpBdd2.sql
+++ TpBdd2.sql
@@ -1,67 +1,3 @@
-
-SELECT * FROM TpBdd2Origen.sys.all_columns;
-SELECT * FROM TpBdd2Origen.sys.all_objects;
-SELECT * FROM TpBdd2Origen.sys.objects;
-SELECT * FROM TpBdd2Origen.sys.schemas;
-SELECT * FROM TpBdd2Origen.sys.system_columns;
-
-SELECT * FROM TpBdd2Origen.sys.tables;
-SELECT * FROM Personas;
-INSERT INTO Personas (Nombre,Apellido) VALUES ('Ezequiel','Sanson'),('Nahuel','Saavedra'),('Joel','Misterio'),('Keko','Incognita'),('Alexis','Quiensabe');
-
-
--- COMO OBTENER LA CANTIDAD DE TABLAS QUE TIENE LA BDD
-DECLARE @cantidadTablas int;
-SET @cantidadTablas = (SELECT COUNT(*) FROM TpBdd2Origen.sys.tables);
-SELECT @cantidadTablas as CantidadTablas;
-
--- COMO OBTENER EL NOMBRE DE LAS TABLAS DE UNA BDD
-DECLARE @nombreTabla varchar(50);
-SET @nombreTabla = (SELECT name FROM TpBdd2Origen.sys.tables WHERE ??);
-SELECT @nombreTabla as NombreTabla;
-
-
--- COMO OBTENGO LA CANTIDAD DE COLUMNAS DE UNA TABLA
-DECLARE @cantidadColumnas int;
-SET @cantidadColumnas = (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Personas');
-SELECT @cantidadColumnas as CantidadColumnas;
-
--- COMO OBTENGO EL NOMBRE DE LOS CAMPOS DE UNA TABLA
-SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Personas';
-
-
-
-
--- ESTO GENERA UN CURSOR QUE RECORRE LA TABLA Y TE TRAE UNO POR UNO LOS RESULTADOS QUE LE PIDAS DE LA TABLA.
-DECLARE @elementoCursor VARCHAR(MAX), @cursorTipo VARCHAR(MAX);
-
-DECLARE contact_cursor CURSOR FOR SELECT Nombre FROM Personas;
-OPEN contact_cursor;   
-FETCH NEXT FROM contact_cursor INTO @elementoCursor; 
--- el INTO le indica en donde quiero poner el resultado que trae el cursor. Tengo que poner la cantidad de variables segun lo que traiga en el SELECT donde declaro el cursor, en este caso una variable sola porque traigo solo el nombre.   
-WHILE @@FETCH_STATUS = 0 -- FETCH_STATUS => Devuelve 0 si esta recorriendo una tabla, devuelve -1 si ya termino.
-BEGIN
-	-- Aca puedo hacer cosas con los datos que dispongo en ese momento
-	select @elementoCursor as Elemento;
-   FETCH NEXT FROM contact_cursor INTO @elementoCursor;  
-END    
-CLOSE contact_cursor;  
-DEALLOCATE contact_cursor;  
-GO  
-
--- Consulta dinamica
-DECLARE @sqlDinamico NVARCHAR(MAX);
-SET @sqlDinamico = 'SELECT Nombre FROM Personas';
-EXECUTE SP_EXECUTESQL @sqlDinamico;
-
-
-
-
-
-
-
-
-
 -------------------------------------------------------------------------------
 ----------------		CREACION DE BASES DE DATOS		-----------------------
 -------------------------------------------------------------------------------
@@ -107,6 +43,9 @@ CREATE TABLE Test.Dueño (
 	CONSTRAINT FK_AutoID FOREIGN KEY (AutoID) REFERENCES Test.Vehiculo(AutoID)
 	);
 GO
+CREATE TABLE Errores (
+	Mensaje VARCHAR(MAX)
+	);
 
 
 INSERT INTO Test.Vehiculo (Modelo, Marca) VALUES ('Gol','VW'),('Onix','Chevrolet'),('208','Peugeot'),('Civic','Honda'),('Clio','Renault');
@@ -158,101 +97,112 @@ AS
 			--DECLARE @BaseOrigen VARCHAR(30)= 'DB_BaseOrigen';
 			--DECLARE @BaseDestino VARCHAR(30)= 'DB_BaseDestino';
 			DECLARE @Nombre_Tabla VARCHAR(500);
-			DECLARE @Nombre_Tabla2 VARCHAR(500);
-			DECLARE @Nombre_Tabla_Comp VARCHAR(500);
+			DECLARE @Schema VARCHAR(500);
+			declare @SchemaResult varchar(500) = '';
 			DECLARE @Nombre_Columna VARCHAR(500);
-			DECLARE @Nombre_Tabla_Comp2 VARCHAR(500);
-			DECLARE @Nombre_Columna2 VARCHAR(500);
-			
 
 			DECLARE @SqlDinamico NVARCHAR(MAX)
-			DECLARE @SqlDinamico2 NVARCHAR(MAX)
-			DECLARE @SqlDinamico3 NVARCHAR(MAX)
-			DECLARE @SqlDinamico4 NVARCHAR(MAX)
+			DECLARE @SchemaQuery NVARCHAR(MAX);
 
-			--El Table_Cursor recorre las tablas de la base origen.
+			--recorre las tablas de la bd 1 
 			SET @SqlDinamico = 'DECLARE Table_Cursor CURSOR FOR SELECT name FROM '+@BaseOrigen+'.sys.tables';
 			EXECUTE SP_EXECUTESQL @sqlDinamico;
 				OPEN Table_Cursor;   
 					FETCH NEXT FROM Table_Cursor INTO @Nombre_Tabla; 
 					WHILE @@FETCH_STATUS = 0 
 					BEGIN
-							--Menciono Cada Tabla
-							SELECT @Nombre_Tabla AS Tabla_Bd_Origen;
-							--Por Cada tabla de la BD_ORIGEN, genero un Cursor que recorra las tablas de la base destino buscando una que se llame igual
-							SET @SqlDinamico2 = 'DECLARE Table_Cursor2 CURSOR FOR SELECT name FROM '+@BaseDestino+'.sys.tables';
-							EXECUTE SP_EXECUTESQL @sqlDinamico2;
-							OPEN Table_Cursor2;   
-								FETCH NEXT FROM Table_Cursor2 INTO @Nombre_Tabla2; 
-								WHILE @@FETCH_STATUS = 0 
-								BEGIN
-									IF(@Nombre_Tabla = @Nombre_Tabla2)
-									--Si encuentra en la BD origen y en la destino dos tablas que se llamen igual
-										BEGIN
-											--Por cada Tabla que Genera este Cursor para recorrer las COLUMNAS de la tabla ORIGEN
-											SET @SqlDinamico3 = 'DECLARE Column_Cursor CURSOR FOR SELECT COLUMN_NAME,TABLE_NAME FROM '+@BaseOrigen+'.INFORMATION_SCHEMA.COLUMNS';
-											EXECUTE SP_EXECUTESQL @sqlDinamico3;
-											OPEN Column_Cursor;   
-												FETCH NEXT FROM Column_Cursor INTO @Nombre_Columna, @Nombre_Tabla_Comp; 
-												WHILE @@FETCH_STATUS = 0 
-												BEGIN
-													--Le agrego un if para que solo entre si estamos hablando de las columnas que de la tabla que estamos recorriendo
-													IF(@Nombre_Tabla = @Nombre_Tabla_Comp)
-														BEGIN
-																--Indico de que tabla estamos hablando
-																--SELECT @Nombre_Columna as ColumnaOrigen;
-																--Por cada columna de la tabla, recorro las columnas de la tabla con el mismo nombre en la base destino
-																SET @SqlDinamico4 = 'DECLARE Column_Cursor2 CURSOR FOR SELECT COLUMN_NAME,TABLE_NAME FROM '+@BaseDestino+'.INFORMATION_SCHEMA.COLUMNS';
-																EXECUTE SP_EXECUTESQL @sqlDinamico4;
-																OPEN Column_Cursor2;   
-																	FETCH NEXT FROM Column_Cursor2 INTO @Nombre_Columna2, @Nombre_Tabla_Comp2; 
-																	WHILE @@FETCH_STATUS = 0 
-																	BEGIN
-																		--Agrego el IF para que filtre solo las columnas de la tabla que estamos recorriendo inicialmente
-																		IF(@Nombre_Tabla = @Nombre_Tabla_Comp2)
-																			BEGIN
-																				--Agrego condicion para que haga algo si las columnas coinciden
-																				IF(@Nombre_Columna = @Nombre_Columna2)
-																					BEGIN
-																						SELECT 'COINCIDEN:',@Nombre_Columna2 as Columna_BD_Destino, @Nombre_Columna as Columna_BD_Origen;
-																						FETCH NEXT FROM Column_Cursor INTO @Nombre_Columna,@Nombre_Tabla_Comp;
-																						--Si tengo Coindicencia de tablas, ya no busco en el resto de registros(esto sirve por ahora, se puede modificar)
-																					END
-																				ELSE
-																					BEGIN
-																						SELECT 'NO COINCIDEN:',@Nombre_Columna2 as Columna_BD_Destino, @Nombre_Columna as Columna_BD_Origen;
-																					END
-																			END
-																		FETCH NEXT FROM Column_Cursor2 INTO @Nombre_Columna2,@Nombre_Tabla_Comp2;
-																	END
-																CLOSE Column_Cursor2;  
-																DEALLOCATE Column_Cursor2;
+						--Guardo el Schema en una variable
+						SET @SchemaQuery = N'SELECT @Schema = TABLE_SCHEMA FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ' + CHAR(39) + @Nombre_Tabla + CHAR(39);
+						EXECUTE SP_EXECUTESQL @SchemaQuery, N'@Schema varchar(500) out', @SchemaResult out
+						--Le paso las tablas para que compare las bases
 
-														END
-													FETCH NEXT FROM Column_Cursor INTO @Nombre_Columna,@Nombre_Tabla_Comp;
-												END
-											CLOSE Column_Cursor;  
-											DEALLOCATE Column_Cursor;
-							
-										END
-									ELSE
-										SELECT @Nombre_Tabla AS SinCoincidencia1, @Nombre_Tabla2 AS SinCoincidencia2
-									FETCH NEXT FROM Table_Cursor2 INTO @Nombre_Tabla2; 
-								END
-							CLOSE Table_Cursor2;  
-							DEALLOCATE Table_Cursor2;
+						EXEC sp_CompareTables @Nombre_Tabla, @BaseOrigen, @BaseDestino, @SchemaResult
 
-						 FETCH NEXT FROM Table_Cursor INTO @Nombre_Tabla;  
-					END    
-			CLOSE Table_Cursor;  
-			DEALLOCATE Table_Cursor;
+					FETCH NEXT FROM Table_Cursor INTO @Nombre_Tabla; 
+					END
+				CLOSE Table_Cursor;  
+				DEALLOCATE Table_Cursor;
 		END
 GO
 ----------------------------------------------------------------------------------------------------
-		
-		
 
+-------------------------------------------------------------------------------
+------------	CREACION DE PROCEDIMIENTO PARA COMPRAR TABLAS	---------------
+-------------------------------------------------------------------------------
+	
+CREATE PROCEDURE sp_CompareTables @Nombre_Tabla VARCHAR(50),@BaseOrigen  VARCHAR(50), @BaseDestino VARCHAR(50), @Schema VARCHAR(50) = NULL
+AS
+DECLARE @Query_Base_Destino VARCHAR(50),
+		@Tabla_Base_Destino NVARCHAR(MAX),
+		@CursorColumnas NVARCHAR(MAX),
+		@CrearTabla  NVARCHAR(MAX),
+		@Tablas_Destino VARCHAR(50),
+		@Nombre_Columna VARCHAR(50),
+		@Tipo_Columna VARCHAR(50),
+		@Longitud VARCHAR(50);
+				
+--Armo la query para ver si existe la tabla en la base destino (Hay que contemplar si tiene schema)
+IF @Schema IS NULL
+	BEGIN
+		SET @Query_Base_Destino = 'SELECT * FROM ' + @BaseDestino + '.' + @Nombre_Tabla;
+	END
+ELSE 
+	BEGIN
+		SET @Query_Base_Destino = 'SELECT * FROM ' + @BaseDestino + '.' + @Schema + '.' + @Nombre_Tabla;
+END
 
+	BEGIN TRY 
+		--Trato de ejecutar la query, si no existe devuelve error (pasa al bloque CATCH)
+		EXEC SP_EXECUTESQL @Query_Base_Destino
+		--Si existe hay que ejecutar procedimiento para comprobar que las columnas sean iguales
+	END TRY
+	BEGIN CATCH
+		INSERT INTO Errores (mensaje) values ( 'La tabla ' + @Nombre_Tabla + ' no existe en ' + @BaseDestino)
+		--Armo un cursor para recorrer los campos que tiene la tabla de la base origen 
+		SET @CursorColumnas = 'DECLARE CursorColumnas CURSOR FOR SELECT COLUMN_NAME, DATA_TYPE,CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ' + CHAR(39) + @Nombre_Tabla + CHAR(39);
+		PRINT 'USE ' + @BaseDestino;
+		EXECUTE SP_EXECUTESQL @CursorColumnas;
+		OPEN CursorColumnas;
+			--Arma la query para crear tabla si no existe
+			FETCH NEXT FROM CursorColumnas INTO @Nombre_Columna, @Tipo_Columna, @Longitud;
+			--Chequeo si tiene Schema
+			IF @Schema IS NULL
+				BEGIN
+					SET @CrearTabla = 'CREATE TABLE [' + @Nombre_Tabla + '] ( [' + @Nombre_Columna + '] [' + @Tipo_Columna + '],';
+				END
+			ELSE 
+				BEGIN
+					PRINT 'CREATE SCHEMA ' + @Schema;
+					SET @CrearTabla = 'CREATE TABLE [' + @Schema +'].[' + @Nombre_Tabla + '] ( [' + @Nombre_Columna + '] [' + @Tipo_Columna + '],';
+				END
+
+			--Si el campo es int la longitud es null
+			IF @Longitud IS NULL
+				BEGIN
+					PRINT @CrearTabla
+				END
+			ELSE 
+				BEGIN
+					PRINT @CrearTabla + ' (' + @Longitud + ')';
+				END
+			FETCH NEXT FROM CursorColumnas INTO @Nombre_Columna, @Tipo_Columna, @Longitud;
+			WHILE @@FETCH_STATUS = 0 
+			BEGIN
+					SET @CrearTabla = '[' + @Nombre_Columna + '] [' + @Tipo_Columna + ']';
+						IF @Longitud IS NULL
+							BEGIN
+								PRINT @CrearTabla + ','
+							END
+						ELSE 
+								BEGIN
+									PRINT @CrearTabla + ' (' + @Longitud + '),';
+								END
+			FETCH NEXT FROM CursorColumnas INTO @Nombre_Columna, @Tipo_Columna, @Longitud;
+			END
+		PRINT ');';
+		CLOSE CursorColumnas;  
+		DEALLOCATE CursorColumnas;
+	END CATCH	
 -------------------------------------------------------------------------------
 -----------------------		EJECUCION		-----------------------------------
 -------------------------------------------------------------------------------
