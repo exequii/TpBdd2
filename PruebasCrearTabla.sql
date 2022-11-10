2bb27b0c57f22e8188bd42589466393fda2015e7
NahuelSavedra <61848207+NahuelSavedra@users.noreply.github.com>
2022-11-10 12:08:46 -0300

    falta evaluar schema

--- PruebasCrearTabla.sql
+++ PruebasCrearTabla.sql
@@ -1,21 +1,10 @@
 DECLARE @BaseOrigen VARCHAR(30)= 'DB_BaseOrigen';
 DECLARE @BaseDestino VARCHAR(30)= 'DB_BaseDestino';
 			DECLARE @Nombre_Tabla VARCHAR(500);
-			DECLARE @Nombre_Tabla2 VARCHAR(500);
-			DECLARE @Nombre_Tabla_Comp VARCHAR(500);
 			DECLARE @Nombre_Columna VARCHAR(500);
-			DECLARE @Nombre_Tabla_Comp2 VARCHAR(500);
-			DECLARE @Nombre_Columna2 VARCHAR(500);
-			
 
 			DECLARE @SqlDinamico NVARCHAR(MAX)
-			DECLARE @SqlDinamico2 NVARCHAR(MAX)
-			DECLARE @SqlDinamico3 NVARCHAR(MAX)
-			DECLARE @SqlDinamico4 NVARCHAR(MAX)
-			DECLARE @Borrar_Columna NVARCHAR(MAX)
 			DECLARE @Crear_Tabla NVARCHAR(MAX)
-
-			DECLARE @CompareTables NVARCHAR(MAX),@CrearTabla NVARCHAR(MAX), @Nombre_Tabla_Destino VARCHAR(500)
 
 --recorre las tablas de la bd 1 
 SET @SqlDinamico = 'DECLARE Table_Cursor CURSOR FOR SELECT name FROM '+@BaseOrigen+'.sys.tables';
@@ -51,49 +40,42 @@ DECLARE @Query_Base_Destino VARCHAR(50),
 SET @Query_Base_Destino = 'SELECT ' + @Nombre_Tabla + ' FROM ' + @BaseDestino + '.sys.tables';
 	BEGIN TRY 
 		--Trato de ejecutar la query, si no existe devuelve error (pasa al bloque CATCH)
-		SELECT @Query_Base_Destino
 		EXEC SP_EXECUTESQL @Query_Base_Destino
 		--Si existe hay que ejecutar procedimiento para comprobar que las columnas sean iguales
 	END TRY
 	BEGIN CATCH
 		--Armo un cursor para recorrer los campos que tiene la tabla de la base origen 
-		SET @CursorColumnas = N'DECLARE CursorColumnas CURSOR FOR SELECT COLUMN_NAME, DATA_TYPE,CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ' + CHAR(39) + @Nombre_Tabla + CHAR(39);
-		SELECT @CursorColumnas
+		SET @CursorColumnas = 'DECLARE CursorColumnas CURSOR FOR SELECT COLUMN_NAME, DATA_TYPE,CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ' + CHAR(39) + @Nombre_Tabla + CHAR(39);
 		EXECUTE SP_EXECUTESQL @CursorColumnas;
 		OPEN CursorColumnas;
-			--Intento armar la query para crear la tabla (por el momento no crea
+			--Arma la query para crear tabla si no existe
 			FETCH NEXT FROM CursorColumnas INTO @Nombre_Columna, @Tipo_Columna, @Longitud;
-			EXECUTE SP_EXECUTESQL @CrearTabla;
+			SET @CrearTabla = N'CREATE TABLE [' + @BaseDestino + '].[' + @Nombre_Tabla + '] ( [' + @Nombre_Columna + '] [' + @Tipo_Columna + ']';
+			IF @Longitud IS NULL
+				BEGIN
+					PRINT @CrearTabla
+				END
+			ELSE 
+				BEGIN
+					PRINT @CrearTabla + ' (' + @Longitud + ')';
+				END
+			FETCH NEXT FROM CursorColumnas INTO @Nombre_Columna, @Tipo_Columna, @Longitud;
 			WHILE @@FETCH_STATUS = 0 
 			BEGIN
-				SELECT @Nombre_Columna, @Tipo_Columna, @Longitud;
-				SET @CrearTabla = N'CREATE TABLE  [' + @BaseDestino + '].[' + @Nombre_Tabla + '] ( [' + @Nombre_Columna + ' ' + @Tipo_Columna + ' ' + @Longitud + '] )';
-				SELECT @CrearTabla;
+					SET @CrearTabla = '[' + @Nombre_Columna + '] [' + @Tipo_Columna + ']';
+						IF @Longitud IS NULL
+							BEGIN
+								PRINT @CrearTabla
+							END
+						ELSE 
+								BEGIN
+									PRINT @CrearTabla + ' (' + @Longitud + ')';
+								END
 			FETCH NEXT FROM CursorColumnas INTO @Nombre_Columna, @Tipo_Columna, @Longitud;
 			END
+		PRINT ');';
 		CLOSE CursorColumnas;  
 		DEALLOCATE CursorColumnas;
 	END CATCH
 
-
-
 EXEC sp_CompareTables 'Dueño', 'DB_BaseOrigen' ,'DB_BaseDestino'
-
-SELECT Dueño FROM DB_BaseDestino.sys.tables
-
-CREATE TABLE [Test].[Dueño](
-	[AutoID] [int] IDENTITY(1,1) NOT NULL,)
-SELECT INTO DB_BaseDestino.Dueño FROM SELECT * FROM DB_BaseOrigen.Dueño)
-
-
-INSERT INTO DB_BaseDestino.Dueño (SELECT * FROM DB_BaseOrigen.Dueño)
-
-SELECT * FROM DB_BaseDestino.Test.Vehiculo
-
-ALTER TABLE DB_BaseDestino.Test.Vehiculo ADD status varchar(1);
-
-SELECT CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Vehiculo';
-
-DECLARE CursorColumnas CURSOR FOR SELECT COLUMN_NAME, DATA_TYPE,CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = Dueño
-
-CREATE TABLE  [DB_BaseDestino].[Dueño] ( [Nombre] [varchar] (30) )
